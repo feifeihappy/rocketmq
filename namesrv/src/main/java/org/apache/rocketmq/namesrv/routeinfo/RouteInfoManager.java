ed8573749138348d5b68d04ef139e1ba1716bafd
赵鹏飞 <vim.zpf@gmail.com>
2025-07-16 17:30:55 +0800

    broker 心跳命令

--- namesrv/src/main/java/org/apache/rocketmq/namesrv/routeinfo/RouteInfoManager.java
+++ namesrv/src/main/java/org/apache/rocketmq/namesrv/routeinfo/RouteInfoManager.java
@@ -49,10 +49,11 @@ public class RouteInfoManager {
     private static final InternalLogger log = InternalLoggerFactory.getLogger(LoggerName.NAMESRV_LOGGER_NAME);
     private final static long BROKER_CHANNEL_EXPIRED_TIME = 1000 * 60 * 2;
     private final ReadWriteLock lock = new ReentrantReadWriteLock();
-    private final HashMap<String/* topic */, List<QueueData>> topicQueueTable;
-    private final HashMap<String/* brokerName */, BrokerData> brokerAddrTable;
-    private final HashMap<String/* clusterName */, Set<String/* brokerName */>> clusterAddrTable;
-    private final HashMap<String/* brokerAddr */, BrokerLiveInfo> brokerLiveTable;
+    //路由信息
+    private final HashMap<String/* topic */, List<QueueData>> topicQueueTable;//Topic 消息队列路由信息
+    private final HashMap<String/* brokerName */, BrokerData> brokerAddrTable;//Broker 节点信息
+    private final HashMap<String/* clusterName */, Set<String/* brokerName */>> clusterAddrTable;//broker 集群信息
+    private final HashMap<String/* brokerAddr */, BrokerLiveInfo> brokerLiveTable;//broker 状态信息
     private final HashMap<String/* brokerAddr */, List<String>/* Filter Server */> filterServerTable;
 
     public RouteInfoManager() {
